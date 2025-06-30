756052c70f735ac56378bb1f31f4c545686b29c7
Suyash <tiwarisuyash700+s@gmail.com>
2025-06-30 21:51:20 +0530

    Practicing day 3 or maybe 4

---
+++ Practical Implementation/loopPractic.java
@@ -0,0 +1,9 @@
+public class loopPractic {
+    public static void main(String[] args) {
+        int n = 2;
+        do { 
+            System.out.println(2*n);
+            n++;
+        } while (n<=10);
+    }
+}
