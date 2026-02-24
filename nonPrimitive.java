fa53f0100c265da0f901c38dab213ca4d7ffedf7
Suyash Tiwari <tiwarisuyash700+s@gmail.com>
2026-02-24 13:00:43 +0530

    reference type practice

--- nonPrimitive.java
+++ nonPrimitive.java
@@ -1,5 +1,11 @@
+import java.awt.Point;
+
 public class nonPrimitive {
     public static void main(String[] args) {
         System.out.println("Hello boy!!");
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
