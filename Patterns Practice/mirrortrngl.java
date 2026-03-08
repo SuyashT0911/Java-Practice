8bf3f1d793182a4d7d63ce6123edaed483c076ef
Suyash Tiwari <tiwarisuyash700+s@gmail.com>
2026-03-08 16:20:30 +0530

    Refactor pattern printing logic and add mirror triangle pattern implementation

---
+++ Patterns Practice/mirrortrngl.java
@@ -0,0 +1,14 @@
+public class mirrortrngl {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++){
+            for (int j = 5; j > i; j--){
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++){
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+    }
+}
--- Patterns Practice/tempCodeRunnerFile.java
+++ Patterns Practice/tempCodeRunnerFile.java
@@ -1,9 +1,11 @@
 class tempCodeRunnerFile {
     public static void main(String[] args) {
         int n = 5; // Example value for n
-        for (int j=0; j<n; j++){
-            System.out.println("*");
-        
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
         }
     }
 }
