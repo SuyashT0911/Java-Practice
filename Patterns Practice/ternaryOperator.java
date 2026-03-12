748e101bd5ee5b270c378e00be6a30b2d38db70d
Suyash Tiwari <tiwarisuyash700+s@gmail.com>
2026-03-12 19:09:21 +0530

    continue prac

---
+++ Patterns Practice/ternaryOperator.java
@@ -0,0 +1,10 @@
+
+public class ternaryOperator {
+
+    public static void main(String[] args) {
+        int x = 8, y = 7;
+
+        int result = (x > y) ? x : y;
+        System.out.println(result);
+    }
+}
---
+++ aif.java
@@ -0,0 +1,24 @@
+
+public class aif {
+
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 7;
+        int z = 9;
+
+        // if (x < y) {
+        // System.out.println(x);
+        // }
+        // else {
+        // System.out.println(y);
+        // }
+        if (x > y && x > z) {
+            System.out.println(x);
+        } else if (y > z) {
+            System.out.println(y);
+        } else {
+            System.out.println(z);
+        }
+
+    }
+}
